eab4aaa28c70f737bac4d7017d02f4d8d8db7672
sabas <sabas88@gmail.com>
2014-06-22 00:22:30 +0200

    top61

---
+++ top61/data/convert.php
@@ -0,0 +1,65 @@
+<?php
+function csvRead($file,$sep)
+{
+	if (($handle = fopen($file, "r")) !== FALSE) {
+		$k=0;
+
+		while (($data = fgetcsv($handle, 1000, $sep)) !== FALSE) {
+			++$k;
+
+			if ($k==1)
+			{
+				$i=-1;
+				foreach ($data as $cell)
+				{
+					$titles[++$i]=$cell;
+				}
+			continue;
+			}
+
+			$i=-1;
+				foreach ($data as $cell)
+				{
+				$csv[$k-2][$titles[++$i]]=$cell;
+				}
+		}
+		fclose($handle);
+	}
+	return $csv;
+}
+
+function csvDump($arr,$sep)
+{
+	$tmp='';
+    $tmp.= implode($sep,array_keys($array))."\n";
+	foreach ($arr as $line)
+	{
+        if (!is_array($line))$tmp.=$line;
+        else
+		$tmp.= implode($sep,$line)."\n";
+    }
+    return 	$tmp;
+}
+
+function csv2geojson($csv,$sep){
+	$in=csvRead($csv,$sep);
+
+	$geo=['type'=>'FeatureCollection'];
+	$geo['features']=array();
+
+	foreach ($in as $row){
+		$f=['type'=>'Feature'];
+		$f['geometry']=['type'=>'Point','coordinates'=>[(float)$row['lon'],(float)$row['lat']]];
+		foreach($row as $k=>$el){
+		if($k=="lon" || $k=="lat") continue;
+		$f['properties'][$k]=$el;
+		}
+	$geo['features'][]=$f;
+	}
+	return json_encode($geo);
+}
+
+$i=$_GET['i'];
+$o=str_replace("csv","geojson",$i);
+file_put_contents($o,csv2geojson($i,"\t"));
+?>
---
+++ top61/data/tool.php
@@ -0,0 +1,84 @@
+<?php
+function csvRead($file,$sep)
+{
+	if (($handle = fopen($file, "r")) !== FALSE) {
+		$k=0;
+
+		while (($data = fgetcsv($handle, 1000, $sep)) !== FALSE) {
+			++$k;
+
+			if ($k==1)
+			{
+				$i=-1;
+				foreach ($data as $cell)
+				{
+					$titles[++$i]=$cell;
+				}
+			continue;
+			}
+
+			$i=-1;
+				foreach ($data as $cell)
+				{
+				$csv[$k-2][$titles[++$i]]=$cell;
+				}
+		}
+		fclose($handle);
+	}
+	return $csv;
+}
+
+function csvDump($arr,$sep)
+{
+	$tmp='';
+    $tmp.= implode($sep,array_keys($array))."\n";
+	foreach ($arr as $line)
+	{
+        if (!is_array($line))$tmp.=$line;
+        else
+		$tmp.= implode($sep,$line)."\n";
+    }
+    return 	$tmp;
+}
+
+$coordf="coordinates.csv";
+
+$file=$_GET['i'];
+
+$sep="\t";
+$coord=csvRead($coordf,$sep);
+$data=csvRead($file,$sep);
+
+$res= fopen("gc_".$file, "w+");
+
+$header=array_keys($data[0]);
+$header[]="lon";
+$header[]="lat";
+fputcsv($res,$header,$sep);
+foreach($data as &$r)
+{
+    $port=findR(trim($r['port']));
+    if($port!=-1)
+	{
+    $r['lon']=$port['lon'];
+    $r['lat']=$port['lat'];
+	}
+    else
+    {
+        echo $r['port']."<br>";
+        $r['lon']="#";
+        $r['lat']="#";
+    }
+
+fputcsv($res,$r,"\t");
+}
+fclose($res);
+
+function findR($p){
+global $coord;
+foreach($coord as $k){
+    if ($k['port']==$p) return $k;
+}
+return -1;
+}
+?>
